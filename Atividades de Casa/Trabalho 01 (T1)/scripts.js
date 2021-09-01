d574dc6905d15fe2d13e8c3c073f561490b6a292
Joao <joaovitor20002@gmail.com>
2021-08-31 22:01:23 -0300

    Trabalho Atualizado

---
+++ Atividades de Casa/Trabalho 01 (T1)/scripts.js
@@ -0,0 +1,7 @@
+function trocarCor(){
+    let corAnterior = "a"
+    let cores = ["red", "cyan", "pink", "blue", "white", "orange", "yellow"]
+    let aleatorio = Math.round(Math.random() * 6)
+    
+    document.body.style.backgroundColor = cores[aleatorio]
+}
