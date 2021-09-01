560b81a5cb32448dddfd23c6f0f3fe4377dbd547
Joao <joaovitor20002@gmail.com>
2021-08-31 22:13:09 -0300

    Trabalho 01 Atualizado

--- Atividades de Casa/Trabalho 01 (T1)/scripts.js
+++ Atividades de Casa/Trabalho 01 (T1)/scripts.js
@@ -1,5 +1,5 @@
 function trocarCor(){
-    let corAnterior = "a"
+let corAnterior = "a"
     let cores = ["red", "cyan", "pink", "blue", "white", "orange", "yellow"]
     let aleatorio = Math.round(Math.random() * 6)
     
