3e808dc9366ea71011427c61f26feaf82b3800c7
Joao <joaovitor20002@gmail.com>
2021-11-08 20:20:38 -0300

    (08/11) Criacao DOM

--- Atividades de Casa/Trabalho 01 (T1)/scripts.js
+++
@@ -1,7 +0,0 @@
-function trocarCor(){
-let corAnterior = "a"
-    let cores = ["red", "cyan", "pink", "blue", "white", "orange", "yellow"]
-    let aleatorio = Math.round(Math.random() * 6)
-    
-    document.body.style.backgroundColor = cores[aleatorio]
-}
